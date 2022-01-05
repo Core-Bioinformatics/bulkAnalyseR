32445e313afc0175f1269590929d849150bf8bf6
elliecwilliams <ecw63@cam.ac.uk>
2022-01-05 17:09:40 +0000

    Fixed quantile normalisation in preprocessExpressionMatrix

--- R/preprocessExpressionMatrix.R
+++ R/preprocessExpressionMatrix.R
@@ -38,6 +38,7 @@ preprocessExpressionMatrix <- function(
     expression.matrix.normalised <- preprocessCore::normalize.quantiles(expression.matrix)
     rownames(expression.matrix.normalised) <- base::rownames(expression.matrix)
     colnames(expression.matrix.normalised) <- base::colnames(expression.matrix)
+    expression.matrix <- expression.matrix.normalised
     message("Done!")
   }else if(normalisation.method[1] == "rpm"){
     message("Performing ", normalisation.method[1], " normalisation...")
